e6fa330d74e6a19f4e83f9e6338862055fd2b259
Damien Mannion <damien@djmannion.net>
2024-11-01 10:56:03 +1100

    -

---
+++ tests/test_log.py
@@ -0,0 +1,21 @@
+
+import os
+import logging
+
+import pytest
+
+import crossref_lmdb.log
+
+
+def test_get_log_level():
+
+    assert crossref_lmdb.log.get_log_level() == logging.INFO
+
+    os.environ["CROSSREF_LMDB_LOG_LEVEL"] = "warning"
+
+    assert crossref_lmdb.log.get_log_level() == logging.WARNING
+
+    os.environ["CROSSREF_LMDB_LOG_LEVEL"] = "yada"
+
+    with pytest.raises(SystemExit):
+        crossref_lmdb.log.get_log_level()
